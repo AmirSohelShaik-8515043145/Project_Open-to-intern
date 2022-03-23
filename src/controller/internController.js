d19868cb9c2abb15e09415abc49e6d2d1805e642
AmirSohelShaik-8515043145 <amirsohelsk07@gmail.com>
2022-03-23 11:13:56 +0530

    changes

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -3,7 +3,7 @@ const collegeModel = require('../model/collegeModel')
 
 
 const isValid = function (value) {
-     if (typeof (value) === undefined || typeof (value) === null) { return false }
+     if (typeof (value) === 'undefined' || typeof (value) === null) { return false }
      if (typeof (value).trim().length == 0) { return false }
      if (typeof (value) === "string" && (value).trim().length > 0){ return true }
  }
